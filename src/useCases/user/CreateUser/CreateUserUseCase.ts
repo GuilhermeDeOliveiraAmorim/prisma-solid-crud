730df600c87a86c5a9381b543d73a8082ebbc24d
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2022-08-25 22:07:07 -0300

    feat: applying SOLID principles

---
+++ src/useCases/user/CreateUser/CreateUserUseCase.ts
@@ -0,0 +1,40 @@
+import { User } from "../../../entities/User";
+import { IMailProvider } from "../../../providers/IMailProvider";
+import { IUserRepository } from "../../../repositories/IUserRepository";
+import { ICreateUserRequestDTO } from "./CreateUserDTO";
+
+class CreateUserUseCase {
+    constructor(
+        private usersRepository: IUserRepository,
+        private mailProvider: IMailProvider
+    ) {}
+
+    async execute(data: ICreateUserRequestDTO) {
+        const userAlreadyExists = await this.usersRepository.findByEmail(
+            data.email
+        );
+
+        if (userAlreadyExists) {
+            throw new Error("User already exists.");
+        }
+
+        const user = new User(data);
+
+        await this.usersRepository.save(user);
+
+        this.mailProvider.sendMail({
+            to: {
+                name: data.name,
+                email: data.email,
+            },
+            from: {
+                name: "Guilherme Amorim",
+                email: "contato@guilhermeamorim.com",
+            },
+            subject: "Seja bem-vindo!",
+            body: "<p>Você agora faz parte!</p>",
+        });
+    }
+}
+
+export { CreateUserUseCase };
